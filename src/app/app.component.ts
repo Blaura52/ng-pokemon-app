f493d28c1b7fea4689f5ca8f94bd665910e7ae1d
Blaura52 <122250894+Blaura52@users.noreply.github.com>
2023-05-11 11:35:12 +0200

    commit-validate-v2-composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,27 @@ import { POKEMONS } from './mock-pokemon-list'; //import de la liste Pokémons q
 import { Pokemon } from './pokemon';  //import de l'object Pokemon
 @Component({
   selector: 'app-root',
-  //1 template: `<h1>Welcome to {{title}}!</h1>` 
-  //2 template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`  Vue, affichage de la liste POKEMONS a l'index 0
-  template: `<h1>Liste de Pokémons</h1>`
+  templateUrl: 'app.component.html' 
 })
 
 export class AppComponent implements OnInit {
-  //1 title = 'application de pokémons';  On passe un nom à notre appli
-  //2 pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];  On passe trois pokémons en entréé en chaine de caractères.
-  //3 pokemonList = POKEMONS;  Constante POKEMONS de l'import de la liste de POKEMONS
   pokemonList: Pokemon[] = POKEMONS; //On type notre propriété pokemonList en disant que c'est un tableau de pokémons, l'utilisateur devra obligatoirement lui passer une liste de pokémons avec des identifiants, point d evie etc..
+  pokemonSelected: Pokemon|undefined; //on raoute le type Undefined pour supprimer notre erreur dans la condition ifelse(pokemon) car en bas on se sers de type Undefined, donc il faut le déclarer ici pour que ça fonctionne
 
-  //constructor() {
-  //   this.pokemonList = []; ❌ très déconseillé de déclarer une variable dans le constructor
-  // }
-
-  ngOnInit() { // ou ngOnInit(): void{}, pas obliger de mettre void
+  ngOnInit() {
     console.table(this.pokemonList);
-    //2 this.selectPokemon('Bulbizarre'); la méthode permet d'afficher une chaine de caractère
-    this.selectPokemon(this.pokemonList[0]); // on ne veut plus une chaine de caractère, mais un object métier Pokemon car en ayant changé le paramètre de la méthode selectPokemon  on ne respecte plus le contrat d'interface
-  }
+  } 
+  
+  selectPokemon(pokemonId: string) { 
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
 
-  //2 selectPokemon(pokemonName: string) { le paramètre de la méthode est une chaine de caractère définit juste en dessous
-    selectPokemon(pokemon: Pokemon) { //comme paramètre on type la propriété avec un objet métier Pokemon 
-    //2 console.log(`Vous avez cliqué sur le pokémon ${pokemonName} propriété d'avant pour récuper le nom en chaine de caractère
-      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`); //on récupère la proprièté .name grâce à l'objet pokemon
-  }
+    if(pokemon){
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
+      this.pokemonSelected = pokemon;
+    }
 }
+}
+ 
