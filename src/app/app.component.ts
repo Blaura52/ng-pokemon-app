029e77bd705b657e6928238a9fa2a44b618e533c
Blaura52 <122250894+Blaura52@users.noreply.github.com>
2023-05-11 19:16:04 +0200

    commit-v3-composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { Pokemon } from './pokemon';  //import de l'object Pokemon
 
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS; //On type notre propriété pokemonList en disant que c'est un tableau de pokémons, l'utilisateur devra obligatoirement lui passer une liste de pokémons avec des identifiants, point d evie etc..
-  pokemonSelected: Pokemon|undefined; //on raoute le type Undefined pour supprimer notre erreur dans la condition ifelse(pokemon) car en bas on se sers de type Undefined, donc il faut le déclarer ici pour que ça fonctionne
+  pokemonSelected: Pokemon | undefined; //on raoute le type Undefined pour supprimer notre erreur dans la condition ifelse(pokemon) car en bas on se sers de type Undefined, donc il faut le déclarer ici pour que ça fonctionne
 
   ngOnInit() {
     console.table(this.pokemonList);
   } 
   
   selectPokemon(pokemonId: string) { 
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
 
     if(pokemon){
       console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
