4b6f5ea65f68c40cc55d9d5799e4611369c07889
Blaura52 <122250894+Blaura52@users.noreply.github.com>
2023-05-12 19:12:24 +0200

    commit-v5-composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import { BorderCardDirective } from './border-card.directive'; //import de BorderCardDirective et création du fichier border-card.directive fait automatiquement par Angular CLI grace a la ligne de commande : ng generate directive border-card
 import { AppRoutingModule } from './app-routing.module';
+import { BorderCardDirective } from './border-card.directive'; //import de BorderCardDirective et création du fichier border-card.directive fait automatiquement par Angular CLI grace a la ligne de commande : ng generate directive border-card
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';//import de PokemonTypeColorPipe et création du fichier pokemon-type-color.pipe fait automatiquement par Angular CLI grace a la ligne de commande : ng generate pipe pokemon-type-color
 
 @NgModule({
   imports: [BrowserModule,AppRoutingModule],
-  declarations: [AppComponent,BorderCardDirective],
+  declarations: [AppComponent,BorderCardDirective, PokemonTypeColorPipe],
   providers: [],
   bootstrap: [AppComponent]
 })
