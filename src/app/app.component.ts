820ca3c4a66d4dc125e16f5f9262b24d07477fb7
Blaura52 <122250894+Blaura52@users.noreply.github.com>
2023-05-13 19:31:28 +0200

    commit-v6-composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,9 @@
 import { Component, OnInit } from '@angular/core'; //import de OnInit pour initialiser notre composant
-import { POKEMONS } from './mock-pokemon-list'; //import de la liste Pokémons que j'ai ajouté précédemment depuis le fichier mock-pokemon-list
-import { Pokemon } from './pokemon';  //import de l'object Pokemon
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html' 
 })
 
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS; //On type notre propriété pokemonList en disant que c'est un tableau de pokémons, l'utilisateur devra obligatoirement lui passer une liste de pokémons avec des identifiants, point d evie etc..
-  pokemonSelected: Pokemon | undefined; //on rajoute le type Undefined pour supprimer notre erreur dans la condition ifelse(pokemon) car en bas on se sers de type Undefined, donc il faut le déclarer ici pour que ça fonctionne
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  } 
-  
-  selectPokemon(pokemonId: number) { //selectPokemon(pokemonId: string)
-    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-
-    if(pokemon){
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-  }
+export class AppComponent {
 }
  
---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; //1 import d'ActivatedRoute d'Angular pour avoir accès à la route courante
+import { POKEMONS } from '../mock-pokemon-list'; //2 pour pourvoir accès à mes pokémons dans ma liste de pokémons
+import { Pokemon } from '../pokemon'; //2 pour avoir accès à ma liste de pokémons
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html',
+})
+export class DetailPokemonComponent implements OnInit {
+  
+  pokemonList: Pokemon[]; //2 on définit une variable avec une liste de pokémons
+  pokemon: Pokemon | undefined; //2 pokemon est le pokémon courant qui correspond au Pokemon qui a été demandé par l'utilisateur via l'URL en identifiant. on a une propriété qui contient le pokemon (courant) à afficher à l'utilisateur
+
+  constructor(private route: ActivatedRoute) {} //1 injection de ActivatedRoute qui viens de l'import {ActivatedRoute} d'angular/router pour avoir accès à la route courante
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS; //2 la propriété pokemonList sera un object de type POKEMONS, ça y est on peut accéder à nos pokémons dans notre liste pokémons
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id'); //1 on déclare une constante pokemonId qui sera un nombre si le pokémon existe sinon elle sera null. On va aussi faire appel au routeur .router pour récupérer cet id; .snapshot permet d'avoir la donnée à l'instant T et des paramètres qui sont transmis sous forme de .paramMap
+    if (pokemonId) { // on attribut à la propriété pokemonId, le pokemon qui lui correspond
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId) //2 pour récupérer le pokemon courant dans la liste de POKEMONS qui correspond au Pokemon demandé par l'URL
+    } // else {
+      //   this.pokemon = undefined;
+      // }   //cela ne sers à rien de le réinitialiser car si le pokemonId n'a pas été trouvé, le pokemon vaut déja undefined par défaut 
+  }
+}
