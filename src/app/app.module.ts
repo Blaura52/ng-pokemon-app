736ba27ca8905ee9906bd052efaa59b2a332f109
Blaura52 <122250894+Blaura52@users.noreply.github.com>
2023-05-14 10:48:58 +0200

    commit-v7-composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BorderCardDirective } from './border-card.directive'; //import de Borde
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';//import de PokemonTypeColorPipe et création du fichier pokemon-type-color.pipe fait automatiquement par Angular CLI grace a la ligne de commande : ng generate pipe pokemon-type-color
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';//import de ListPokemonComponent et création du Dossier list-pokemon avec 2 fichiers list-pokemon.component, un .html, l'autre .ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component list-pokemon --inline-template=false
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';//import de DetailPokemonComponent et création du Dossier detail-pokemon avec 2 fichiers detail-pokemon.component, un .html, l'autre .ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component detail-pokemon --inline-template=false
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';//import de PageNotFoundComponent et création du Dossier page-not-found avec 1 fichiers page-not-found.component.ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component page-not-found. Je n'ai pas ajouté --inline-template=false à la fin de la ligne de commande car je ne voulais pas un fichier .ts et un .html, il n'y a pas besoin que le template soin à part
 @NgModule({
   imports: [
     BrowserModule,
@@ -16,7 +17,8 @@ import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.componen
     BorderCardDirective, 
     PokemonTypeColorPipe, 
     ListPokemonComponent, 
-    DetailPokemonComponent],
+    DetailPokemonComponent, 
+    PageNotFoundComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'; //1 import d'ActivatedRoute d'Angular pour avoir accès à la route courante
+import { ActivatedRoute, Router } from '@angular/router'; //1 import d'ActivatedRoute d'Angular pour avoir accès à la route courante
 import { POKEMONS } from '../mock-pokemon-list'; //2 pour pourvoir accès à mes pokémons dans ma liste de pokémons
 import { Pokemon } from '../pokemon'; //2 pour avoir accès à ma liste de pokémons
 
@@ -12,7 +12,8 @@ export class DetailPokemonComponent implements OnInit {
   pokemonList: Pokemon[]; //2 on définit une variable avec une liste de pokémons
   pokemon: Pokemon | undefined; //2 pokemon est le pokémon courant qui correspond au Pokemon qui a été demandé par l'utilisateur via l'URL en identifiant. on a une propriété qui contient le pokemon (courant) à afficher à l'utilisateur
 
-  constructor(private route: ActivatedRoute) {} //1 injection de ActivatedRoute qui viens de l'import {ActivatedRoute} d'angular/router pour avoir accès à la route courante
+  constructor(private route: ActivatedRoute, private router: Router) {} //1 injection du service ActivatedRoute qui viens de l'import {ActivatedRoute} d'angular/router pour avoir accès à la route courante
+                                                                        //3 injection du service Router qui va nous permettre d'avoir accès au routeur
 
   ngOnInit(): void {
     this.pokemonList = POKEMONS; //2 la propriété pokemonList sera un object de type POKEMONS, ça y est on peut accéder à nos pokémons dans notre liste pokémons
@@ -22,5 +23,9 @@ export class DetailPokemonComponent implements OnInit {
     } // else {
       //   this.pokemon = undefined;
       // }   //cela ne sers à rien de le réinitialiser car si le pokemonId n'a pas été trouvé, le pokemon vaut déja undefined par défaut 
+  }
+
+  goToPokemonList() { //ma méthode sur laquelle est dirigé l'utilisateur quand on intercepte l'évènement (click) 
+    this.router.navigate(['/pokemons']); //on fait appel au routeur dans notre méthode, pour accéder à sa propriété .navigate et en paramètres on a plus qu'à passer l'URL de redirection /pokemons pour revenir à la liste des pokèmonss
   }
 }
