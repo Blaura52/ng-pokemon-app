c5e557409e4cad20d7ef17effb3d839c8f314107
Blaura52 <122250894+Blaura52@users.noreply.github.com>
2023-05-18 11:51:42 +0200

    commit-v8-services

--- src/app/app.module.V4.ts
+++ src/app/app.module.V4.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive'; //import de BorderCardDirective et création du fichier border-card.directive fait automatiquement par Angular CLI grace a la ligne de commande : ng generate directive border-card
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';//import de PokemonTypeColorPipe et création du fichier pokemon-type-color.pipe fait automatiquement par Angular CLI grace a la ligne de commande : ng generate pipe pokemon-type-color
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';//import de ListPokemonComponent et création du Dossier list-pokemon avec 2 fichiers list-pokemon.component, un .html, l'autre .ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component list-pokemon --inline-template=false
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';//import de DetailPokemonComponent et création du Dossier detail-pokemon avec 2 fichiers detail-pokemon.component, un .html, l'autre .ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component detail-pokemon --inline-template=false
-@NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule],
-  declarations: [
-    AppComponent,
-    BorderCardDirective, 
-    PokemonTypeColorPipe, 
-    ListPokemonComponent, 
-    DetailPokemonComponent],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { BorderCardDirective } from './pokemon/border-card.directive'; //import de BorderCardDirective et création du fichier border-card.directive fait automatiquement par Angular CLI grace a la ligne de commande : ng generate directive border-card
+// import { PokemonTypeColorPipe } from './pokemon/pokemon-type-color.pipe';//import de PokemonTypeColorPipe et création du fichier pokemon-type-color.pipe fait automatiquement par Angular CLI grace a la ligne de commande : ng generate pipe pokemon-type-color
+// import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component';//import de ListPokemonComponent et création du Dossier list-pokemon avec 2 fichiers list-pokemon.component, un .html, l'autre .ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component list-pokemon --inline-template=false
+// import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component';//import de DetailPokemonComponent et création du Dossier detail-pokemon avec 2 fichiers detail-pokemon.component, un .html, l'autre .ts fait automatiquement par Angular CLI grace a la ligne de commande : ng generate component detail-pokemon --inline-template=false
+// @NgModule({
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule],
+//   declarations: [
+//     AppComponent,
+//     BorderCardDirective, 
+//     PokemonTypeColorPipe, 
+//     ListPokemonComponent, 
+//     DetailPokemonComponent],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'; //1 import d'ActivatedRoute d'Angular pour avoir accès à la route courante
-import { POKEMONS } from '../mock-pokemon-list'; //2 pour pourvoir accès à mes pokémons dans ma liste de pokémons
-import { Pokemon } from '../pokemon'; //2 pour avoir accès à ma liste de pokémons
+//3 import { POKEMONS } from '../mock-pokemon-list'; //2 pour pourvoir accès à mes pokémons dans ma liste de pokémons
+import { Pokemon } from '../pokemon'; //2 pour avoir accès à ma liste de pokémons 
+import { PokemonService } from '../pokemon.service';//3 import pour l'instançation de mon nouveau service 
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -12,15 +13,20 @@ export class DetailPokemonComponent implements OnInit {
   pokemonList: Pokemon[]; //2 on définit une variable avec une liste de pokémons
   pokemon: Pokemon | undefined; //2 pokemon est le pokémon courant qui correspond au Pokemon qui a été demandé par l'utilisateur via l'URL en identifiant. on a une propriété qui contient le pokemon (courant) à afficher à l'utilisateur
 
-  constructor(private route: ActivatedRoute, private router: Router) {} //1 injection du service ActivatedRoute qui viens de l'import {ActivatedRoute} d'angular/router pour avoir accès à la route courante
-                                                                        //3 injection du service Router qui va nous permettre d'avoir accès au routeur
+  constructor(
+    private route: ActivatedRoute,//1 injection du service ActivatedRoute qui viens de l'import {ActivatedRoute} d'angular/router pour avoir accès à la route courante
+    private router: Router, //2 injection du service Router qui va nous permettre d'avoir accès au routeur
+    private pokemonService: PokemonService //3 instançation de mon nouveau service 
+    ) {} 
 
   ngOnInit(): void {
-    this.pokemonList = POKEMONS; //2 la propriété pokemonList sera un object de type POKEMONS, ça y est on peut accéder à nos pokémons dans notre liste pokémons
+    //3 this.pokemonList = POKEMONS; //2 la propriété pokemonList sera un object de type POKEMONS, ça y est on peut accéder à nos pokémons dans notre liste pokémons
     const pokemonId: string | null = this.route.snapshot.paramMap.get('id'); //1 on déclare une constante pokemonId qui sera un nombre si le pokémon existe sinon elle sera null. On va aussi faire appel au routeur .router pour récupérer cet id; .snapshot permet d'avoir la donnée à l'instant T et des paramètres qui sont transmis sous forme de .paramMap
     if (pokemonId) { // on attribut à la propriété pokemonId, le pokemon qui lui correspond
-      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId) //2 pour récupérer le pokemon courant dans la liste de POKEMONS qui correspond au Pokemon demandé par l'URL
-    } // else {
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+      //3 this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId) //2 pour récupérer le pokemon courant dans la liste de POKEMONS qui correspond au Pokemon demandé par l'URL
+    } 
+    // else {
       //   this.pokemon = undefined;
       // }   //cela ne sers à rien de le réinitialiser car si le pokemonId n'a pas été trouvé, le pokemon vaut déja undefined par défaut 
   }
